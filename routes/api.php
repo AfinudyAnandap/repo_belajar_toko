829c7ff92b6c25c2507c5e710d6c5a9d43a126c1
AfinudyAnanadap <git@afinudy_ananda_28rpl@student.smktelkom-mlg.sch.id>
2020-12-14 21:20:39 +0700

    Selesai

--- routes/api.php
+++ routes/api.php
@@ -7,27 +7,36 @@ Route::post('/login', 'UserController@login');
 
 
 Route::group(['middleware' => ['jwt.verify']], function (){
-    Route::group(['middleware' => ['api.superadmin']], function (){
+
+    Route::group(['middleware' => ['api.superadmin']], function ()
+    {
+        Route::delete('/customers/{id}', 'customers@destroy');
+        Route::delete('/product/{id}', 'product@destroy');
+        Route::delete('/order/{id}', 'order@destroy');
+    });
+
+    Route::group(['middleware' => ['api.admin']], function ()
+        {
+            Route::post('/order', 'order@store');
+            Route::put('/order/{id}', 'order@update');
+            Route::post('/prodct', 'product@store');
+            Route::put('/product/{id}', 'product@update');
+            Route::post('/customers', 'customer@store');
+            Route::put('/customers/{id}', 'customers@update');
 
     });
 
-    Route::group(['middleware' => ['api.admin']], function (){
-
-    });
-
-   Route::post('/order', 'order@store');
+   
    Route::get('/order', 'order@show');
    Route::get('/order/{id}', 'order@detail');
-   Route::delete('/order/{id}', 'order@destroy');
+  
 
-   Route::post('/product', 'product@store');
    Route::get('/product', 'product@show');
    Route::get('/product/{id}', 'product@detail');
-   Route::delete('/product/{id}', 'product@destroy');
-
+  
    Route::get('/customers', 'customer@show');
    Route::get('/customers/{id}', 'customer@detail');
-   Route::post('/customers', 'customer@store');
-   Route::put('/customers/{id}', 'customers@update');
-   Route::delete('/customers/{id}', 'customers@destroy');
-});
+  
+  
+}
+);
